bdc358caa38aa81037da31e8813da0d3e4ee9bce
Estevão P. Rolim <estevao.prolim@gmail.com>
2018-07-25 21:08:21 -0300

    Major changes in front-end

--- js/main.js
+++ js/main.js
@@ -1,100 +1,216 @@
-var isMobile = true;
-
 //var validaHTML = '<table><tr><th>Claridad <b>(1 - 5)</b></th></tr><tr><td><div class="opts"><div><b>1</b><br><input type="radio" name="brad" value="1" required><br>Confuso</div><div><b>2</b><br><input type="radio" name="brad" value="2"><br>Un poco confuso</div><div><b>3</b><br><input type="radio" name="brad" value="3"><br>Compreensible</div><div><b>4</b><br><input type="radio" name="brad" value="4"><br>Muy claro</div><div><b>5</b><br><input type="radio" name="brad" value="5"><br>Totalmente claro</div></div></td></tr></table><table><tr><th>Comentarios <b>(opcional)</b></th></tr><tr><td><textarea name="bcom" id="comentario" maxlength="280" placeholder="Feu clic o toqueu aquí per escriure text."></textarea></td></tr></table>';
 
 $(function () {
 	$("form").sisyphus();
 });
 
-var adap = function adaptiveLayout() {
 
-	if (screen.height < screen.width) {
-		isMobile = false;
-	} else {
-		isMobile = true;
-	}
-
-
-	var select = document.getElementsByClassName('select');
-	var short = document.getElementsByClassName('short');
-	var value = document.getElementsByClassName('value');
-	var opts = document.getElementsByClassName('opts');
-	var valida = document.getElementsByClassName('valida');
-	var item = document.getElementsByClassName('item');
-	var leg_b = document.getElementsByClassName('leg_b');
-	var half_grid = document.getElementsByClassName('half_grid');
-	var consentimiento = document.getElementById('consentimiento');
-	var aviso = document.getElementsByClassName('aviso');
-	var avalia = document.getElementsByClassName('avalia');
-
-	// Mobile friendly code
-	if (isMobile) {
-		for (var i = 0; i < select.length; i++) {
-			select[i].style = 'width: 94%;';
-		}
-		for (var i = 0; i < short.length; i++) {
-			short[i].style = 'width: 90%;';
-		}
-		for (var i = 0; i < value.length; i++) {
-			value[i].style = 'width: 45%;';
-		}
-		for (var i = 0; i < opts.length; i++) {
-			opts[i].style = '';
-		}
-		for (var i = 0; i < item.length; i++) {
-			item[i].style = '';
-		}
-		for (var i = 0; i < valida.length; i++) {
-			valida[i].style = '';
-		}
-		for (var i = 0; i < leg_b.length; i++) {
-			leg_b[i].style = '';
-		}
-		for (var i = 0; i < half_grid.length; i++) {
-			half_grid[i].style = '';
-		}
-		consentimiento.style = 'width: 100%;';
-		for (var i = 0; i < aviso.length; i++) {
-			aviso[i].style = '';
-		}
-		for (var i = 0; i < avalia.length; i++) {
-			avalia[i].style = '';
-		}
-	} else {
-		for (var i = 0; i < select.length; i++) {
-			select[i].style = '';
-		}
-		for (var i = 0; i < short.length; i++) {
-			short[i].style = '';
-		}
-		for (var i = 0; i < value.length; i++) {
-			value[i].style = '';
-		}
-		for (var i = 0; i < opts.length; i++) {
-			opts[i].style = 'grid-template-columns: 19% 19% 19% 19% 20%; grid-column-gap: 1%;';
-		}
-		for (var i = 0; i < item.length; i++) {
-			item[i].style = 'grid-template-columns: 49% 49%; grid-column-gap: 2%;';
-		}
-		for (var i = 0; i < valida.length; i++) {
-			valida[i].style = 'grid-template-columns: 73% 26%; grid-column-gap: 1%;';
-		}
-		for (var i = 0; i < leg_b.length; i++) {
-			leg_b[i].style = 'font-size: 16px;';
-		}
-		consentimiento.style = '';
-		for (var i = 0; i < half_grid.length; i++) {
-			half_grid[i].style = 'grid-template-columns: 49% 49%; grid-column-gap: 2%;';
-		}
-		for (var i = 0; i < aviso.length; i++) {
-			aviso[i].style = 'grid-template-columns: 5% 93%; grid-column-gap: 2%;';
-		}
-		for (var i = 0; i < avalia.length; i++) {
-			avalia[i].style = 'font-size: 12px;';
-		}
+//// classes para ajustes da disposição em celular (vertical e horizontal) e PC
+//
+//var content = document.getElementsByClassName('content');
+//var select = document.getElementsByClassName('select');
+//var short = document.getElementsByClassName('short');
+//var value = document.getElementsByClassName('value');
+//var opts = document.getElementsByClassName('opts');
+//var valida = document.getElementsByClassName('valida');
+//var item = document.getElementsByClassName('item');
+//var leg_b = document.getElementsByClassName('leg_b');
+//var half_grid = document.getElementsByClassName('half_grid');
+//var consente = document.getElementsByClassName('consente');
+//var aviso = document.getElementsByClassName('aviso');
+//var imgInfo = document.getElementsByClassName('imgInfo');
+//var avalia = document.getElementsByClassName('avalia');
+//var ubLogo = document.getElementsByClassName('ubLogo');
+//var dropdown = document.getElementsByClassName('dropdown');
+//var dropdown_content = document.getElementsByClassName('dropdown_content');
+//var show = document.getElementsByClassName('show');
+//
+//
+//function isVerticalMobile() {
+//	for (var i = 0; i < content.length; i++) {
+//		content[i].style = '';
+//	}
+//	for (var i = 0; i < select.length; i++) {
+//		select[i].style = 'width: 94%;';
+//	}
+//	for (var i = 0; i < short.length; i++) {
+//		short[i].style = 'width: 90%;';
+//	}
+//	for (var i = 0; i < value.length; i++) {
+//		value[i].style = 'width: 45%;';
+//	}
+//	for (var i = 0; i < opts.length; i++) {
+//		opts[i].style = '';
+//	}
+//	for (var i = 0; i < item.length; i++) {
+//		item[i].style = '';
+//	}
+//	for (var i = 0; i < valida.length; i++) {
+//		valida[i].style = '';
+//	}
+//	for (var i = 0; i < leg_b.length; i++) {
+//		leg_b[i].style = '';
+//	}
+//	for (var i = 0; i < half_grid.length; i++) {
+//		half_grid[i].style = '';
+//	}
+//	for (var i = 0; i < aviso.length; i++) {
+//		aviso[i].style = '';
+//	}
+//	for (var i = 0; i < imgInfo.length; i++) {
+//		imgInfo[i].style = 'padding-show:0';
+//	}
+//	for (var i = 0; i < avalia.length; i++) {
+//		avalia[i].style = '';
+//	}
+//	for (var i = 0; i < consente.length; i++) {
+//		consente[i].style = 'width: 100%';
+//	}
+//	for (var i = 0; i < ubLogo.length; i++) {
+//		ubLogo[i].style = 'width: 95%';
+//	}
+//	for (var i = 0; i < dropdown.length; i++) {
+//		dropdown[i].style = 'display: inline-block;';
+//	}
+//	for (var i = 0; i < show.length; i++) {
+//		show[i].style = 'display: none;';
+//	}
+//}
+//
+//function isHorizontalMobile() {
+//	for (var i = 0; i < content.length; i++) {
+//		content[i].style = '';
+//	}
+//	for (var i = 0; i < select.length; i++) {
+//		select[i].style = '';
+//	}
+//	for (var i = 0; i < short.length; i++) {
+//		short[i].style = '';
+//	}
+//	for (var i = 0; i < value.length; i++) {
+//		value[i].style = '';
+//	}
+//	for (var i = 0; i < opts.length; i++) {
+//		opts[i].style = 'grid-template-columns: 19% 19% 19% 19% 20%; grid-column-gap: 1%;';
+//	}
+//	for (var i = 0; i < item.length; i++) {
+//		item[i].style = 'grid-template-columns: 49% 49%; grid-column-gap: 2%;';
+//	}
+//	for (var i = 0; i < valida.length; i++) {
+//		valida[i].style = 'grid-template-columns: 73% 26%; grid-column-gap: 1%;';
+//	}
+//	for (var i = 0; i < leg_b.length; i++) {
+//		leg_b[i].style = '';
+//	}
+//	for (var i = 0; i < consente.length; i++) {
+//		consente[i].style = '';
+//	}
+//	for (var i = 0; i < half_grid.length; i++) {
+//		half_grid[i].style = 'grid-template-columns: 49% 49%; grid-column-gap: 2%;';
+//	}
+//	for (var i = 0; i < aviso.length; i++) {
+//		aviso[i].style = 'grid-template-columns: 7% 92%; grid-column-gap: 1%;';
+//	}
+//	for (var i = 0; i < imgInfo.length; i++) {
+//		imgInfo[i].style = '';
+//	}
+//	for (var i = 0; i < avalia.length; i++) {
+//		avalia[i].style = '';
+//	}
+//	for (var i = 0; i < ubLogo.length; i++) {
+//		ubLogo[i].style = 'width: 90%';
+//	}
+//	for (var i = 0; i < dropdown.length; i++) {
+//		dropdown[i].style = 'display: inline-block;';
+//	}
+//	for (var i = 0; i < show.length; i++) {
+//		show[i].style = 'display: none;';
+//	}
+//}
+//
+//function notMobile() {
+//	for (var i = 0; i < content.length; i++) {
+//		content[i].style = 'width: 80%;';
+//	}
+//	for (var i = 0; i < select.length; i++) {
+//		select[i].style = '';
+//	}
+//	for (var i = 0; i < short.length; i++) {
+//		short[i].style = '';
+//	}
+//	for (var i = 0; i < value.length; i++) {
+//		value[i].style = '';
+//	}
+//	for (var i = 0; i < opts.length; i++) {
+//		opts[i].style = 'grid-template-columns: 1fr 1fr 1fr 1fr 1fr; grid-column-gap: 1%;';
+//	}
+//	for (var i = 0; i < item.length; i++) {
+//		item[i].style = 'grid-template-columns: 49% 49%; grid-column-gap: 2%;';
+//	}
+//	for (var i = 0; i < valida.length; i++) {
+//		valida[i].style = 'grid-template-columns: 73% 26%; grid-column-gap: 1%;';
+//	}
+//	for (var i = 0; i < leg_b.length; i++) {
+//		leg_b[i].style = 'font-size: 16px;';
+//	}
+//	for (var i = 0; i < consente.length; i++) {
+//		consente[i].style = '';
+//	}
+//	for (var i = 0; i < half_grid.length; i++) {
+//		half_grid[i].style = 'grid-template-columns: 49% 49%; grid-column-gap: 2%;';
+//	}
+//	for (var i = 0; i < aviso.length; i++) {
+//		aviso[i].style = 'grid-template-columns: 7% 92%; grid-column-gap: 1%;';
+//	}
+//	for (var i = 0; i < imgInfo.length; i++) {
+//		imgInfo[i].style = '';
+//	}
+//	for (var i = 0; i < avalia.length; i++) {
+//		avalia[i].style = '';
+//	}
+//	for (var i = 0; i < ubLogo.length; i++) {
+//		ubLogo[i].style = 'width: 70%';
+//	}
+//	for (var i = 0; i < dropdown.length; i++) {
+//		dropdown[i].style = '';
+//	}
+//	for (var i = 0; i < show.length; i++) {
+//		show[i].style = '';
+//	}
+//}
+//
+//var adap = function adaptiveLayout() {
+//
+//	if (screen.height < screen.width) {
+//		if (screen.width < 1200)
+//			isHorizontalMobile();
+//		else
+//			notMobile();
+//	} else {
+//		isVerticalMobile();
+//	}
+//}
+//
+//window.onload = adap;
+//
+//window.onresize = adap;
+//
+//
+function dropMenu() {
+	for (var i = 0; i < dropdown_content.length; i++) {
+		dropdown_content[i].classList.toggle("dropShow");
 	}
 }
 
-window.onload = adap;
+window.onclick = function (event) {
+	if (!event.target.matches('.dropdown_btn')) {
 
-window.onresize = adap;
+		var dropdowns = dropdown_content;
+		for (var i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('dropShow')) {
+				openDropdown.classList.remove('dropShow');
+			}
+		}
+	}
+}
--- js/mapa.js
+++ js/mapa.js
@@ -4,7 +4,7 @@ function setup() {
 	var diameter = (2 / 3) * width;
 	var maxStroke = width / 60;
 	var minStroke = width / 300;
-
+	var npoints = 8;
 
 	createCanvas(width, height);
 
@@ -25,7 +25,7 @@ function setup() {
 	translate(width / 2, height / 2 + width / 20);
 
 
-	textAlign(LEFT);
+	textAlign(RIGHT);
 
 	stroke(0);
 	strokeWeight(minStroke);
@@ -34,16 +34,16 @@ function setup() {
 
 	stroke('#000');
 	strokeWeight(minStroke);
-	angleLimits(0, 0, diameter / 2 - diameter / 100, 6);
+	angleLimits(0, 0, diameter / 2 - diameter / 100, npoints);
 
 	stroke('#c78009');
 	strokeWeight(maxStroke);
 	fill(220);
-	polygon(0, 0, diameter / 2 - diameter / 100, 6, POINTS);
+	polygon(0, 0, diameter / 2 - diameter / 100, npoints, POINTS);
 
-	habilidades = ["Ataque", "Defesa", "Drible", "Fintas", "Chute", "Velocidade"]
+	habilidades = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII"]
 
-	polLabel(0, 0, diameter / 2, 6, habilidades, 0);
+	polLabel(0, 0, diameter / 2, npoints, habilidades, 0);
 
 	var ata = 87;
 	ata = map(ata, 0, 100, 0, diameter / 2);
@@ -63,12 +63,13 @@ function setup() {
 	var vel = 80;
 	vel = map(vel, 0, 100, 0, diameter / 2);
 
-	posHab = [ata, def, dri, fin, chu, vel];
+	posHab = [ata, def, dri, fin, chu, vel, 90, 76];
+
 
 	fill(190, 21, 33, 70);
 	stroke(190, 21, 33);
 	strokeWeight(minStroke);
-	habPolygon(0, 0, 6, posHab);
+	habPolygon(0, 0, npoints, posHab);
 }
 
 function polygon(x, y, radius, npoints, type) {
@@ -104,27 +105,6 @@ function polLabel(x, y, radius, npoints, labels, color) {
 	for (var a = 0; a < TWO_PI; a += angle) {
 		var tx = x + cos(a) * radius;
 		var ty = y + sin(a) * radius;
-
-		if (a == 0) {
-			tx += fontsize * 1 / 2;
-			ty += fontsize * 2 / 6;
-
-		} else if (a < HALF_PI) {
-			ty += fontsize * 3 / 2;
-			tx -= fontsize * 2 / 3;
-		} else if (a < PI) {
-			ty += fontsize * 3 / 2;
-			tx -= fontsize * 3 / 2;
-		} else if (a == PI) {
-			ty += fontsize * 2 / 7;
-			tx -= fontsize * 10 / 3;
-		} else if (a < 3 * HALF_PI) {
-			tx -= fontsize * 2;
-			ty -= fontsize * 2 / 3;
-		} else if (a > 3 * HALF_PI) {
-			tx += fontsize / 2;
-			ty -= fontsize * 1 / 3;
-		}
 
 		text(lbl[i], tx, ty);
 		i++;
